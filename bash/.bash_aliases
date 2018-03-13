cd929ba70933c79758bb5b956dcf893647e31334
The Yyg <theyyg@hotmail.com>
2018-03-13 12:48:44 -0400

    Adding i3 configuration

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -2,7 +2,6 @@
 
 # Program aliases
 alias chrome=chromium-browser
-alias ssh-dev='ssh -X mldl4047'
 
 alias less='less --RAW-CONTROL-CHARS'
 export LS_OPTS='--color=auto'
--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -3,12 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat)))
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(inhibit-startup-screen t)
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa-stable" . "https://stable.melpa.org/packages/") ("melpa-latest" . "https://melpa.org/packages/")))))
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa-stable" . "https://stable.melpa.org/packages/") ("melpa-latest" . "https://melpa.org/packages/"))))
+ '(sr-speedbar-right-side nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "PragmataPro for Powerline")))))
