9ee6dd39d3080e5a1b3f5100d61dc5ffa162fe86
Blaine Wood <bwood@magicleap.com>
2021-05-27 16:35:29 -0600

    Emacs config update and run permissions
    Run permissions added to scripts in bin/ by default
    Added plantuml.jar for easy installation
    
    Emacs config
     Adding default grep command
     Updated counsel keybinds and config
     Tabbing config update
     Ligature temporarily disabled
     Customized Org-todos
     Updated org documentation with examples of file variables and headers
     Added shell as a babel language
     Adding cpp files to automatically enable c-or-c++-mode
     Finished adding old emacs config to org-config
    
    yyg-custom plantuml style updates
     audref added
     ctlref added
     ClearWrap added
     ClearPack added

--- emacs27/.emacs-custom.el
+++ emacs27/.emacs-custom.el
@@ -3,39 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(browse-url-browser-function 'browse-url-chrome)
- '(custom-safe-themes
-   '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
- '(display-time-mode t)
- '(doom-modeline-mode t)
- '(flycheck-global-modes nil)
- '(flymake-google-cpplint-command "/usr/local/bin/cpplint")
- '(flymake-google-cpplint-filter "--filter=-whitespace/line_length,-build")
- '(flymake-google-cpplint-verbose "--verbose=0")
+ '(cursor-in-non-selected-windows 'hollow)
+ '(cursor-type 'box)
  '(grep-command "grep --color -RinH -B 2 -A 2 -e ")
- '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
- '(package-archives
-   '(("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-latest" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(sublimity gerrit gerrit-download flycheck git-commit yasnippet yasnippet-snippets slack python-mode dash realgud flycheck-clang-analyzer flycheck-clang-tidy doom doom-modeline doom-themes octicons spaceline spaceline-all-the-icons spacemacs-theme ob-browser ob-html-chrome ob-http ob-ipython ein nlinum graphviz-dot-mode plantuml-mode matlab-mode gnu-elpa-keyring-update dap-mode lsp-treemacs lsp-ui magit-gerrit hide-lines company-lsp cquery lsp-mode clang-format google-c-style flymake-google-cpplint flymake-cursor ace-window magit magit-filenotify magit-find-file magit-lfs magit-org-todos magit-todos))
- '(shift-select-mode nil)
- '(size-indication-mode t)
- '(sr-speedbar-right-side nil)
- '(sublimity-mode nil)
- '(tab-width 4)
- '(telephone-line-mode t)
- '(whitespace-style
-   '(face trailing tabs spaces lines newline empty indentation::tab space-after-tab space-before-tab space-mark tab-mark newline-mark))
- '(zoom-size '(80 . 0.618)))
-
+   '(org-plus-contrib company-box dap-mode lsp-treemacs lsp-ivy counsel-projectile projectile visual-fill-column org-bullets helpful counsel ivy-rich ivy which-key general yasnippet-snippets use-package sublimity spacemacs-theme spaceline-all-the-icons slack realgud python-mode plantuml-mode octicons ob-ipython ob-http ob-html-chrome ob-browser nlinum matlab-mode magit-todos magit-org-todos magit-lfs magit-gerrit magit-filenotify lsp-ui hide-lines graphviz-dot-mode google-c-style gerrit-download gerrit flymake-google-cpplint flymake-cursor flycheck-clang-tidy flycheck-clang-analyzer ein doom-themes doom-modeline doom cquery company-lsp clang-format ace-window))
+ '(safe-local-variable-values '((org-confirm-babel-evaluate))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-
-
